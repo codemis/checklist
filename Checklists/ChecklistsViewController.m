5546d34681a5d51525037760e6b7f80726c374f9
Bruce Hobbs <brucehobbs@engineeredsw.com>
2013-03-09 15:22:47 -0800

    Create enough rows so there's actually some scrolling. Apply StackOverflow info to set/get BOOL. Retrieved item/dictionary should be mutable. Update item/dictionary when row is selected. Note question regarding for loop curly brace formatting.

--- Checklists/ChecklistsViewController.m
+++ Checklists/ChecklistsViewController.m
@@ -1,7 +1,7 @@
 #import "ChecklistsViewController.h"
 
-@interface ChecklistsViewController ()<UITableViewDataSource,
-UITableViewDelegate>
+@interface ChecklistsViewController()
+  <UITableViewDataSource,UITableViewDelegate>
 @property(strong, nonatomic)NSMutableArray *items;
 @end
 
@@ -10,9 +10,14 @@ UITableViewDelegate>
 {
     [super viewDidLoad];
     self.items = [[NSMutableArray alloc] init];
-    NSArray *titles = @[@"Walk the dog", @"Feed the cat", @"Feed the zombie", @"Feed the cat the zombie"];
-    for (NSString *title in titles) {
-        NSMutableDictionary *item = [@{@"title": title, @"isChecked": @"NO"} mutableCopy];
+    NSArray *titles = @[@"Walk the dog", @"Feed the cat", @"Feed the zombie",
+                        @"Feed the cat the zombie",@"Wash the dog",
+                        @"Feed the dog",@"Walk the cat",@"Walk the zombie",
+                        @"Wash the zombie",@"Wash the cat",@"Water the lawn",
+                        @"Mow the lawn",@"Wash the car",@"Take out the trash"];
+    for (NSString *title in titles) { // Why is this curly brace on this line?
+        NSMutableDictionary *item = [@{@"title": title,
+                                     @"isChecked": @NO} mutableCopy];
         [self.items addObject:item];
     }
 }
@@ -24,10 +29,12 @@ numberOfRowsInSection:(NSInteger)section
 -(UITableViewCell *)tableView:(UITableView *)tableView
         cellForRowAtIndexPath:(NSIndexPath *)indexPath
 {
-    UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:@"ChecklistItem"];
-    NSDictionary *item = self.items[indexPath.row];
+    UITableViewCell *cell =
+      [tableView dequeueReusableCellWithIdentifier:@"ChecklistItem"];
+    NSMutableDictionary *item = self.items[indexPath.row];
     cell.textLabel.text = item[@"title"];
-    cell.accessoryType = [item[@"isChecked"] isEqual: @"YES"] ? UITableViewCellAccessoryCheckmark : UITableViewCellAccessoryNone;
+    cell.accessoryType = [item[@"isChecked"] boolValue] ?
+      UITableViewCellAccessoryCheckmark : UITableViewCellAccessoryNone;
     return cell;
 }
 -(void)         tableView:(UITableView *)tableView
@@ -36,8 +43,10 @@ didSelectRowAtIndexPath:(NSIndexPath *)indexPath
     UITableViewCell *cell = [tableView cellForRowAtIndexPath:indexPath];
     if (cell.accessoryType == UITableViewCellAccessoryNone) {
         cell.accessoryType = UITableViewCellAccessoryCheckmark;
+        self.items[indexPath.row][@"isChecked"] = @YES;
     } else {
         cell.accessoryType = UITableViewCellAccessoryNone;
+        self.items[indexPath.row][@"isChecked"] = @NO;
     }
     [tableView deselectRowAtIndexPath:indexPath animated:YES];
 }
